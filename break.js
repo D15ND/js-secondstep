beaeac5e43712275d0ecd4fa65c7bd651a0c167f
ILYA <106765406+D15ND@users.noreply.github.com>
2022-09-22 21:18:56 +0300

    add

---
+++ break.js
@@ -0,0 +1,9 @@
+let a=0;
+while(a<5) {
+    if (a===1) {
+        break;
+    }
+    a++
+    console.log(a);
+}
+
---
+++ ff.js
@@ -0,0 +1,4 @@
+for (let a=0; a<10; a++) {
+    if (a%2==0)continue;
+        console.log(a);
+}
