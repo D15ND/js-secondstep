387dbc29114653b6acbe74938bdef3d34a0b8620
ILYA <106765406+D15ND@users.noreply.github.com>
2022-09-26 21:18:13 +0300

    add loweranduppercase

---
+++ seminame.js
@@ -0,0 +1,5 @@
+let usName='aERo folDer';
+console.log(usName);
+console.log(usName.slice(0,4));
+console.log(usName.toLowerCase());
+console.log(usName[0].toLocaleUpperCase()+usName.toLowerCase().slice(1));
