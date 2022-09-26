17c22e8a051e222818802c4488a1b0ea89e83c5b
ILYA <106765406+D15ND@users.noreply.github.com>
2022-09-26 17:22:56 +0300

    add je

---
+++ asasda.js
@@ -0,0 +1,24 @@
+function switchItUp(number){
+    switchItUp=(6);
+    if (switchItUp===0) {
+      return('Zero');
+    } else if (switchItUp===1) {
+      return('One');
+    } else if (switchItUp===2) {
+      return('Two');
+    } else if (switchItUp===3) {
+      return('Three');
+    } else if (switchItUp===4) {
+      return('Four');
+    } else if (switchItUp===5) {
+      return('Five');
+    } else if (switchItUp===6) {
+      return('Six');
+    } else if (switchItUp===7) {
+      return('Seven');
+    } else if (switchItUp===8) {
+      return('Eight');
+    } else if (switchItUp===9) {
+      return('Nine');
+    }
+}
