39ff55661a747ca83dc2d09b98f4009e1f8d9814
ILYA <106765406+D15ND@users.noreply.github.com>
2022-09-28 17:22:12 +0300

    add js

---
+++ sum.js
@@ -0,0 +1,38 @@
+function greet(language) {
+	if (greet=='english') {
+    return('Welcome');
+    } else if (greet=='czech') {
+      return('Vitejte'); 
+    } else if (greet=='danish') {
+      return('Velkomst');
+    } else if (greet=='dutch') {
+      return('Welkom');
+    } else if (greet=='estonian') {
+      return('Tere tulemast');
+    } else if (greet=='finnish') {
+      return('Tervetuloa');
+    } else if (greet=='flemish') {
+      return('Welgekomen');
+    } else if (greet=='french') {
+      return('Bienvenue');
+    } else if (greet=='german') {
+      return('Willkommen');
+    } else if (greet=='irish') {
+      return('Failte');
+    } else if (greet=='italian') {
+      return('Benvenuto');
+    } else if (greet=='latvian') {
+      return('Gaidits');
+    } else if (greet=='lithuanian') {
+      return('Laukiamas');
+    } else if (greet=='polish') {
+      return('Witamy');
+    } else if (greet=='spanish') {
+      return('Bienvenido');
+    } else if (greet=='swedish') {
+      return('Valkommen');
+    } else if (greet=='welsh') {
+      return('Croeso');
+    }
+}
+greet('welsh');
