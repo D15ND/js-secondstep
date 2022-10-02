eb9816c1e005fab2a024e58dd2cbad506b06a364
ILYA <106765406+D15ND@users.noreply.github.com>
2022-10-02 18:40:09 +0300

    add switch-case

---
+++ s.js
@@ -0,0 +1,20 @@
+function labSwi(val) {
+    let answer="";
+    switch (val) {
+        case 1:
+            answer='one';
+            break;
+        case 2:
+            answer='two';
+            break;
+        case 3:
+            answer='three';
+            break;
+        case 4:
+            answer='four';
+            break;
+    }
+    return answer;
+}
+
+labSwi(2);
